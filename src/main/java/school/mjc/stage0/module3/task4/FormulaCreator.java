576d1d3864d3f32cb98b426259415b01113c9a1f
Aidin <bekturganovaidin@gmail.com>
2023-06-28 14:05:05 +0600

    finished tasks

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        float a = 1f;
+        float b = 1f;
+
+        double ans = (9 * (a * a) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
     }
 }
--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+
+        int charC = 99;
+        System.out.println((char) charC);
+
+        int charH = 104;
+        System.out.println((char) charH);
+
+        int charA = 97;
+        System.out.println((char) charA);
+
+        int charR = 114;
+        System.out.println((char) charR);
     }
 }
